e65ccdf7c875f4bc6263d7625cc3cffbb0f33af2
I_Iskuzhin <44976339+Lemeri02@users.noreply.github.com>
2019-08-27 22:15:40 +0500

    v2
    Добавил кейсы

--- test/loop.test.js
+++ test/loop.test.js
@@ -1,31 +1,20 @@
 const { get } = require('axios');
 require('should');
 const headers = { 'Content-Type' : 'application/json'};
-const cases = [
-    {n: 1, xs: 99},
-    {n: 2, xs: 255},
-    {n: 3, xs: 483},
-    {n: 4, xs: 783},
-    {n: 5, xs: 1155},
-    {n: 1050230, xs: 39707440315455},
-    {n: 6.2, xs: 1696.4400000000003},
-]; 
 
-//const n = 1;
-//const URL = `http://kodaktor.ru/api2/there/${n}`;
-//get(URL, {headers})
-//    .then(({ data : x }) => console.log(x))
-
-
-cases.forEach(({n, xs }) => 
-    describe(xs + ' asyncAdd', ()=> 
+const array = [];
+for (let i=-10; i<10; i++){
+    array.push(i);
+}
+  
+array.forEach((n) => 
+    describe(n + ' asyncAdd', ()=> 
         it ('should respond with a number', async () => {
             const urlThere = `http://kodaktor.ru/api2/there/${n}`;
             const { data : x } = await get(urlThere, { headers })
             const urlAndba = `http://kodaktor.ru/api2/andba/${x}`;
             const { data : y } = await get(urlAndba, { headers })
-
-            x.should.equal(xs);
+ 
             y.should.equal(n);
         })
     )
